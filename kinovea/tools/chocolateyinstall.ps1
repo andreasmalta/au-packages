67093f217696df27a68ce5fab6780148e3a55cc8
Andreas Andersson <16372461+andreasmalta@users.noreply.github.com>
2021-10-18 16:40:33 +0200

    kinovea

--- kinovea/tools/chocolateyinstall.ps1
+++ kinovea/tools/chocolateyinstall.ps1
@@ -1,13 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
-$url            = 'https://www.kinovea.org/setup/kinovea.0.9.5/Kinovea-0.9.5-x64.exe'
-$checksum       = '489FBCC5006B97830511490B251772BBCC7D23689CB8A635B8BF75590C2B7656'
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  softwareName  = 'kinovea*'
+  packageName   = 'kinovea'
   fileType      = 'exe'
-  url           = $url
-  checksum      = $checksum
+  url           = 'https://www.kinovea.org/setup/kinovea.0.9.5/Kinovea-0.9.5-x64.exe'
+  checksum      = '489fbcc5006b97830511490b251772bbcc7d23689cb8a635b8bf75590c2b7656'
   checksumType  = 'sha256'
   silentArgs    = "/S"
   validExitCodes= @(0, 3010, 1641)
--- kinovea/update.ps1
+++ kinovea/update.ps1
@@ -3,31 +3,25 @@
 $domain   = 'https://www.kinovea.org'
 $releases = "$domain/download.html"
 
-
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
       "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
       "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
-      "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
     }
-    ".\librecad.nuspec" = @{
+    ".\kinovea.nuspec" = @{
       "\<releaseNotes\>.+" = "<releaseNotes>$($Latest.ReleaseNotes)</releaseNotes>"
     }
   }
 }
 
-#https://www.kinovea.org/setup/kinovea.0.9.5/Kinovea-0.9.5-x64.exe
-
 function global:au_GetLatest {
+
   $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
-
-  $re    = '\.exe$'
-  $url   = $download_page.links | ? href -match $re | select -First 1 -expand href
-
-  $version  = ($url -split '/' | select -Last 1 -Skip 1)
-
-  $releaseNotesUrl = "$domain/LibreCAD/LibreCAD/releases/tag/" + $version
+  $url = $download_page.links | ? href -match '\.exe$' | select -First 1 -expand href
+  $version = ($url -split '/' | select -Last 1 -Skip 1)
+  $version = $version -replace "kinovea.", ""
+  $releaseNotesUrl = $domain + "/setup/kinovea." + $version + "/Changelog." + $version + ".txt"
 
   @{
     URL32 = $domain + $url
